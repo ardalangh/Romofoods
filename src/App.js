432403e0a7c2dacc3adb91217d769233ecbfc080
ardalangh <rdghoorchian@gmail.com>
2022-02-03 14:24:19 -0800

    Header updated

--- src/App.js
+++ src/App.js
@@ -2,16 +2,23 @@ import './App.css';
 import ProductModel from './components/ProductModel';
 import {Route, Routes} from 'react-router-dom';
 import HeaderBar from './components/HeaderBar';
+import Box from '@mui/material/Box';
 
 
 function App() {
     return (
-        <div className="App">
+        <Box
+            className="App"
+            sx={{
+                display: 'flex',
+                height: "100vh"
+            }}
+        >
             {/*<Routes>*/}
             {/*    <Route path="/" element={<ProductModel/>} />*/}
             {/*</Routes>*/}
             <HeaderBar/>
-        </div>
+        </Box>
     );
 }
 
--- src/components/HeaderBar.js
+++ src/components/HeaderBar.js
@@ -12,6 +12,9 @@ import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import {useState} from 'react';
+import logo from '../logo.png';
+import HorizontalRuleOutlinedIcon from '@mui/icons-material/HorizontalRuleOutlined';
+import {Divider} from '@material-ui/core';
 
 
 const pages = ['Products', 'Pricing', 'Blog'];
@@ -37,46 +40,108 @@ const HeaderBar = () => {
 	};
 
 	return (
-		<AppBar position="static">
-			<Container maxWidth="xl">
-				<Toolbar disableGutters>
 
-				<Box sx={{flexGrow: 1, display: {xs: 'none', md: 'flex', justifyContent: 'center'}}}>
-					<Button
-						onClick={handleCloseNavMenu}
-						sx={{my: 2, color: 'white', display: 'block'}}
-					>
-						{'page'}
-					</Button>
-					<Button
-						onClick={handleCloseNavMenu}
-						sx={{my: 2, color: 'white', display: 'block'}}
-					>
-						{'page'}
-					</Button>
-					<Typography
-						variant="h6"
-						noWrap
-						component="div"
-						sx={{mr: 2, display: {xs: 'none', md: 'flex'}}}
-					>
-						LOGO
-					</Typography>
-					<Button
-						onClick={handleCloseNavMenu}
-						sx={{my: 2, color: 'white', display: 'block'}}
-					>
-						{'page'}
-					</Button>
-					<Button
-						onClick={handleCloseNavMenu}
-						sx={{my: 2, color: 'white', display: 'block'}}
-					>
-						{'page'}
-					</Button>
+			<Box
+				sx={{
+					display: 'flex',
+					flexDirection: 'column',
+					justifyContent: 'center',
+					padding: '0 5%',
+					background: `url(${logo}) scroll right bottom no-repeat`,
+
+				}}
+			>
+				<Box sx={{
+					display: 'flex',
+					justifyContent: 'center',
+				}}>
+					<HorizontalRuleOutlinedIcon sx={{display: 'inline-block'}}/>
+					<Typography sx={{
+						display: 'inline-block',
+						fontFamily: 'Space Mono, monospace',
+						margin: '0%',
+						letterSpacing: '0.2rem',
+						fontWeight: 'bolder',
+					}}>ROMO FOODS</Typography>
+					<HorizontalRuleOutlinedIcon sx={{display: 'inline-block'}}/>
 				</Box>
-				</Toolbar>
-			</Container>
-		</AppBar>);
+
+
+				<Button>
+					<Typography sx={{fontFamily: 'Space Mono, monospace',  color:'#8e8e8e'}}>Contact us</Typography>
+				</Button>
+				<Button>
+					<Typography sx={{fontFamily: 'Space Mono, monospace',  color:'#8e8e8e'}}>Locations</Typography>
+				</Button>
+				<Button>
+					<Typography sx={{fontFamily: 'Space Mono, monospace', color:'#8e8e8e'}}>Nutrients</Typography>
+				</Button>
+
+			</Box>
+
+
+	);
 };
 export default HeaderBar;
+
+{/*<Toolbar disableGutters>*/
+}
+{/*	<Box sx={{flexGrow: 1, display: {xs: 'flex', md: 'flex', justifyContent: 'center'}}}>*/
+}
+{/*		<Button*/
+}
+{/*			onClick={handleCloseNavMenu}*/
+}
+{/*			sx={{my: 2, color: '#598525', display: 'block'}}*/
+}
+{/*		>*/
+}
+{/*			{'page'}*/
+}
+{/*		</Button>*/
+}
+{/*		<Button*/
+}
+{/*			onClick={handleCloseNavMenu}*/
+}
+{/*			sx={{my: 2, color: '#598525', display: 'block'}}*/
+}
+{/*		>*/
+}
+{/*			{'page'}*/
+}
+{/*		</Button>*/
+}
+
+{/*		<img src={logo} width="100px"/>*/
+}
+{/*			<*/
+}
+{/*				Button*/
+}
+{/*				onClick={handleCloseNavMenu}*/
+}
+{/*				sx={{my: 2, color: '#598525', display: 'block'}}*/
+}
+{/*			>*/
+}
+{/*				{'page'}*/
+}
+{/*			</Button>*/
+}
+{/*			<Button*/
+}
+{/*				onClick={handleCloseNavMenu}*/
+}
+{/*				sx={{my: 2, color: '#598525', display: 'block'}}*/
+}
+{/*			>*/
+}
+{/*				{'page'}*/
+}
+{/*			</Button>*/
+}
+{/*	</Box>*/
+}
+{/*</Toolbar>*/
+}
