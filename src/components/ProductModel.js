5ecd6d69586cdd6d18c5829334928498c33e4ece
ardalangh <rdghoorchian@gmail.com>
2022-02-08 16:36:57 -0800

    uploaded a 3d model in our canvas

--- src/components/ProductModel.js
+++ src/components/ProductModel.js
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react';
 import * as THREE from "three";
-
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 function ProductModel(props) {
 
@@ -18,20 +19,31 @@ function ProductModel(props) {
 		renderer.setSize( window.innerWidth * 0.8, window.innerHeight);
 
 
+		const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.9 );
+		directionalLight.position.z = 5
+		directionalLight.position.x = 5
+		scene.add( directionalLight );
+		// const loader = new GLTFLoader().setPath( '/public/DamagedHelmet/glTF' );
+		const loader = new GLTFLoader()
+		loader.load( 'scene.gltf', function ( gltf ) {
+			gltf.scene.position.y = -1.5
+			scene.add(gltf.scene);
 
-		const geometry = new THREE.BoxGeometry();
-		const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-		const cube = new THREE.Mesh( geometry, material );
-		scene.add( cube );
+
+		})
 
 		camera.position.z = 5;
+		camera.position.y = 1;
+		camera.lookAt(0,0,0)
+
+
+
+		const controls = new OrbitControls( camera, renderer.domElement );
 
 
 		function animate() {
 			requestAnimationFrame( animate );
 
-			cube.rotation.x += 0.01;
-			cube.rotation.y += 0.01;
 
 			renderer.render( scene, camera );
 		};
